6a91355a43a787b8ba806e3497cdce62cd422285
ArthurSerafini <arthurrcserafini@gmail.com>
2026-09-04 07:46:15 -0300

    api status.js finished

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,9 +1,17 @@
 import database from "infra/database.js"
 
 async function status(request, response) {
-    const result = await database.query('SELECT 7+7;');
-    console.log(result)
-    response.status(200).json({"chave":"conteúdo"})
+    
+    var databaseName = process.env.POSTGRES_DATABASE
+    const dbResponseOpenedConnections = await database.query({
+        text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+        values: [databaseName]
+    });
+    console.log(dbResponseOpenedConnections.rows)
+
+    response.status(200).json({
+        opened_connections: dbResponseOpenedConnections.rows[0].count
+    });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,7 @@
 test("GET api status", async () => {
     const response = await fetch ("http://localhost:3000/api/v1/status")
-    expect(response.status).toBe(200)
+    expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+    console.log(responseBody)
 })
